c08785cf94abcb5c67d4e566046b021fe4aecb58
rogerc-sad <63436235+rogerc-sad@users.noreply.github.com>
2025-06-22 23:47:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('¡Hola, desde Node.js!');
+  res.end('Hola, desde Node.js!!!!!');
 });
 
 server.listen(port, hostname, () => {
